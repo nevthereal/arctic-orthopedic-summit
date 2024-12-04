c38d44c3b2fde4347a17e7d831e4514c3bac4a73
Neville Brem <bremneville@gmail.com>
2024-12-04 20:08:52 +0100

    update mail

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
